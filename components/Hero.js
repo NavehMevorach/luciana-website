c640a131e404ea6aaca45193307de0e0e8b39e30
Naveh Mevorach <navehmevorach@gmail.com>
2022-03-27 10:34:07 +0300

    fixes

--- components/Hero.js
+++ components/Hero.js
@@ -137,7 +137,7 @@ function Hero({ openModal, closeModal }) {
 
       <video
         className={`w-screen h-screen -z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover`}
-        autoplay
+        autoPlay
         muted
         loop
         playsinline>
