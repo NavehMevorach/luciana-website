e73d458b7d8e975a9b5f846e18edf8517cf6791b
Naveh Mevorach <navehmevorach@gmail.com>
2022-03-28 22:19:27 +0300

    maybe

--- components/Hero.js
+++ components/Hero.js
@@ -135,10 +135,7 @@ function Hero({ openModal, closeModal }) {
         loop
         playsinline
         muted></video> */}
-      <VideoEmbeded
-        src="/assets/luciana.mp4"
-        className="w-screen h-screen -z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover"
-      />
+      <VideoEmbeded />
       {/* <video
         className={`w-screen h-screen -z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover`}
         autoPlay
